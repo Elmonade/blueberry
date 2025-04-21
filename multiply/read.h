1987e6df9cc1ee7e1ea58397c0e7e5da9fa6b4c4
jello <bb222mf@student.lnu.se>
2025-04-21 16:00:58 +0200

    Add repeater

--- multiply/read.h
+++ multiply/read.h
@@ -12,5 +12,6 @@ struct PlotData {
 double readDoubleFromCSV(const std::string &filename, double* arr, int num_doubles);
 void writeMatrixToCSV(const double* matrix, const char* filename, int rows, int cols);
 void writePlotDataToCSV(const std::vector<PlotData>& plotData, const char* filename);
+void writeTimingToCSV(const std::string& filename, double timeA, double timeB);
 
 #endif
